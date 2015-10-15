d4cc8d087e1d243c94ae2155ef5f0c1cc6ab6a5c
hcz <h.cz@qq.com>
2015-10-15 22:49:53 +0800

    add predictPosition and predictRotation

--- game.physics.js
+++ game.physics.js
@@ -1,5 +1,16 @@
 'use strict';
 
+CANNON.Body.prototype.predictPosition = function (delta) {
+    return this.position.vadd(this.velocity.mult(delta));
+};
+
+CANNON.Body.prototype.predictRotation = function (delta) {
+    var euler = new CANNON.Vec3(0, 0, 0);
+    this.quaternion.toEuler(euler);
+
+    return euler.vadd(this.angularVelocity.mult(delta));
+};
+
 var World = function (onsimulate) {
     // cannon.js world
 
