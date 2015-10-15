a648d3442154fa2a7f96fbe3106f88ebf9aed6dc
hcz <h.cz@qq.com>
2015-10-15 19:57:24 +0800

    add physics

--- game.physics.js
+++ game.physics.js
@@ -1,3 +1,29 @@
 'use strict';
 
-// TODO
+var World = function (onsimulate) {
+    // cannon.js world
+
+    var world = new CANNON.World();
+
+    // simulation loop
+
+    world.timeStep = 100;
+    world.timeLast = new Date().getTime();
+
+    world.onsimulate = onsimulate;
+
+    world.simulate = function () {
+        var time = new Date().getTime();
+
+        if (world.timeLast + world.timeStep < time) {
+            world.onsimulate();
+
+            world.step(0.001 * world.timeStep);
+            world.timeLast += world.timeStep;
+        }
+
+        return time - world.timeLast;
+    };
+
+    return world;
+};
